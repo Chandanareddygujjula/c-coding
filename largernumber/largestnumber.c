4a6d51971c1dbd19354169e8897e834c6fcaf52d
chandanareddygujjula <chandanareddygujjula@gmail.com>
2025-04-27 11:48:06 +0530

    Largest Number

---
+++ largernumber/largestnumber.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("\nEnter first number:");
+    scanf("%d",&a);
+    printf("\nEnter second number:");
+    scanf("%d",&b);
+    printf("\nEnter third number:");
+    scanf("%d",&c);
+    int largest=a;
+    if(b>largest)
+    {
+        largest=b;
+    }
+    if(c>largest)
+    {
+        largest=c;
+    }
+    printf("The largest value is : %d",largest);
+    return 0;
+}
